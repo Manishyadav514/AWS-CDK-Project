3220457d908c9c4ec4a810611070cc6b4b912850
Manish <manish.yadav@myglamm.com>
2022-11-22 09:34:20 +0530

    lambda-hello

--- lib/aws-cdk-project-stack.ts
+++ lib/aws-cdk-project-stack.ts
@@ -1,5 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+// import { aws_s3 as s3 } from "aws-cdk-lib";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkProjectStack extends cdk.Stack {
@@ -12,5 +14,19 @@ export class AwsCdkProjectStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'AwsCdkProjectQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // define S3 bucket in stack using bucket construct
+    // new s3.Bucket(this, "AWSCDKProjectBucket", {
+    //   versioned: true,
+    //   removalPolicy: cdk.RemovalPolicy.DESTROY,
+    //   autoDeleteObjects: true,
+    // });
+
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X, // execution environment
+      code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
+    });
   }
 }
