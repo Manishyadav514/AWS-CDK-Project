ddea44fa09fd3e02937be14973dbb40566adc21d
Manish <manish.yadav@myglamm.com>
2022-11-23 12:01:08 +0530

    pipeline-repo-initialize

--- bin/aws-cdk-project.ts
+++ bin/aws-cdk-project.ts
@@ -2,8 +2,10 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AwsCdkProjectStack } from '../lib/aws-cdk-project-stack';
+import { WorkshopPipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
+new WorkshopPipelineStack(app, 'CdkWorkshopPipelineStack');
 new AwsCdkProjectStack(app, 'AwsCdkProjectStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
