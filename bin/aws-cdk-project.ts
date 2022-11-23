c77e7e5a6a0dd850916305aed4510b5fac6cd876
Manish <manish.yadav@myglamm.com>
2022-11-23 13:39:38 +0530

    Add deploy stage to pipeline

--- bin/aws-cdk-project.ts
+++ bin/aws-cdk-project.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AwsCdkProjectStack } from '../lib/aws-cdk-project-stack';
+// import { AwsCdkProjectStack } from '../lib/aws-cdk-project-stack';
 import { WorkshopPipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
